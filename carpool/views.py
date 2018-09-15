4330e60641359597cee7a1574140a9dfde66c369
varunvaruns9 <varunsinghs2021@gmail.com>
2018-09-15 17:07:22 +0530

    carpool.views: Add function to check for college email

--- carpool/views.py
+++ carpool/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 
+
+def IITmail(request):
+    s = request.POST['email']
+    str = s[-14:]
+    if str.lower() == "iitmandi.ac.in":
+        return True
+    else:
+        return False
+
+
 def new(request):
     return
 
+
 def dashboard(request):
     return
+
 
 def addPool(request):
     return
