6af9c40ac9757d684e712d7b038b5abc355a9e6c
varunvaruns9 <varunsinghs2021@gmail.com>
2018-09-15 15:46:20 +0530

    Change a few settings -Set allowed hosts to all -Set time zone for India

--- proj/settings.py
+++ proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+*tep)uv1-1cg0rri7qh!jpf&63tm1#n7*3@5o7+i-55ntaaw4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
