0ca27798d7948094cbb9344151776e152096cb4a
varunvaruns9 <varunsinghs2021@gmail.com>
2018-09-16 12:36:01 +0530

    Add signup view and token generator

--- carpool/views.py
+++ carpool/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.http import HttpResponseRedirect
+from django.contrib.auth.forms import AuthenticationForm as LoginForm
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+from django.contrib.sites.shortcuts import get_current_site
+from .tokens import account_activation_token
+
+from .forms import SignUpForm, PoolForm
 
 
 def IITmail(request):
@@ -11,7 +20,33 @@ def IITmail(request):
 
 
 def new(request):
-    return
+    error=""
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if not IITmail(request):
+            error+="Please use an IIT Mandi email."
+        elif not form.is_valid():
+            error+="Invalid information/ Email already in use."
+        else:
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your account.'
+            message = render_to_string('acc_active_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': user.pk,
+                'token': account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(mail_subject, message, to=[to_email])
+            email.send()
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form, 'error': error, })
 
 
 def dashboard(request):
